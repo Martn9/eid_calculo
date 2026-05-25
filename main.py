e0e347753c74e4745201f89c647703bda654fc1e
bliempi2025 <bliempi2025@alu.uct.cl>
2026-05-25 14:11:48 -0400

    creo main.py como punto de entrada a la app

---
+++ main.py
@@ -0,0 +1,8 @@
+from gui.app_window import AppWindow
+
+if __name__ == "__main__":
+    # 1. Instanciar la ventana principal de la interfaz gráfica
+    app = AppWindow()
+    
+    # 2. Iniciar el bucle principal para mantener la aplicación ejecutándose
+    app.mainloop()
